d20ca0aac9ea99783afbbe03974c6601c640444d
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2020-11-04 00:16:14 +0700

    Add files via upload

---
+++ tugas6-lab2.py
@@ -0,0 +1,11 @@
+a=int(input("Masukkan Nilai a = "))
+b=int(input("Masukkan Nilai b = "))
+print("Variable a = ", a)
+print("Variable b = ", b)
+print("Hasil penggabungan {1} & {0} = %d".format(a,b) % (a+b))
+
+#Konversi nilai variable
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan {1} + {0} = %d".format(a,b) % (a+b))
+print("Hasil pembagian {1} / {0} = %d".format(a,b) % (a/b))
