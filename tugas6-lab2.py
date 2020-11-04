a20827535e7026d53b5cc20c838f2195e7b3a49f
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2020-11-04 11:46:33 +0700

    Update tugas6-lab2.py

--- tugas6-lab2.py
+++ tugas6-lab2.py
@@ -1,11 +1,11 @@
-a=int(input("Masukkan Nilai a = "))
-b=int(input("Masukkan Nilai b = "))
+a = input("Masukkan Nilai a = ")
+b = input("Masukkan Nilai b = ")
 print("Variable a = ", a)
 print("Variable b = ", b)
-print("Hasil penggabungan {1} & {0} = %d".format(a,b) % (a+b))
+print("Hasil penggabungan {0} & {1} = %s".format(a,b) % (a+b))
 
-#Konversi nilai variable
-a=int(a)
-b=int(b)
-print("Hasil penjumlahan {1} + {0} = %d".format(a,b) % (a+b))
-print("Hasil pembagian {1} / {0} = %d".format(a,b) % (a/b))
+# Konversi nilai variable ke dalam integer
+a = int(a)
+b = int(b)
+print("Hasil penjumlahan {0} + {1} = %d".format(a,b) % (a+b))
+print("Hasil pembagian {0} / {1} = %d".format(a,b) % (a/b))
